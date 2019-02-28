5bf58aa079ba4e304e3ab8b25f8b73f0bd08c84f
adamlevesley <47597651+adamlevesley@users.noreply.github.com>
2019-02-28 19:59:38 +0000

    Add files via upload

---
+++ Bottlenose Dolphin Bathymetric graph.R
@@ -0,0 +1,35 @@
+#Packages that I need
+
+
+library(tidyverse)
+library(marmap)
+library(dplyr)
+library(SDMTools)
+library(rgdal)
+library(mapproj)
+
+
+#---------Read in the Data-----------------
+
+
+Effort <- read_csv("AdamLevesley_EffortSightings.csv")
+#----------------------------------------------
+
+
+
+#------------- Convert coordinate data------------------
+head(Effort)
+coordinates(Effort)<-~LonS+LatS
+head(Effort)
+plot(Effort)
+proj4string(Effort)<- "+proj=utm +zone=30 ellps=WGS84"
+Effort <- spTransform(Effort, CRS("+proj=longlat +datum=WGS84"))
+Effort <- data.frame(Effort)
+head(Effort)
+
+#-----------Coordinates have now been converted into WGS84 format--------------------
+
+Effort %>%slice(1924:8171)
+Effort
+
+
